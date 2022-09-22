aeec65571458120c959609b8828585cf0c50cc3a
Teddy Cubaka <teddycubaka96@gmail.com>
2022-09-22 16:01:08 +0100

    add react-rooter-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import RobotHome from './components/games/robot/RobotHome';
 
 function App() {
   return (
-    <>
-      {/* <TextArea /> */}
-      <RobotHome />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<RobotHome />} />
+        <Route path='/robot' element={<RobotHome />} />
+      </Routes>
+    </Router>
     );
 }
 
--- src/components/games/robot/RobotHome.jsx
+++ src/components/games/robot/RobotHome.jsx
@@ -9,7 +9,31 @@ export default function RobotHome (){
         <RobotContext.Provider value={{robotImage, robots}}>
             <h2>Hey ! Wanna play a game ?</h2>
             <p>Then, create any robot you.</p>
-            <button onClick={()=>{console.log(robots)}}>Click on me</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Vos robots</th>
+                        <th>Les robots par default</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <td>Créer un robot</td>
+                        <td>Voir la list des robots</td>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    {
+                        robotImage.map((image, index)=>(
+                            <tr key={index}>
+                                <td>{index+1}</td>
+                                <td><img src={image} alt="" className='img-test'/></td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+
         </RobotContext.Provider>
     </>
 }
