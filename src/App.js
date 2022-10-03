fd3558f6eaec27e68273115c5852731302d1bce6
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-03 16:27:43 +0100

    home config$

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import TextArea from "./components/clipboard/clipboard";
 import CreateRobot from "./components/games/robot/createRobot";
-import RobotHome from "./components/games/robot/RobotHome";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Link to="/" className="back_home">Back home</Link>
         <Routes>
-          <Route path="/" element={<RobotHome />} />
+          <Route path="/" element={<Home />} />
           <Route path="/robot" element={<CreateRobot />} />
+          <Route path="/clipboard" element={<TextArea />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home (){
+    return (
+        <div className="disp_column">
+            <h2>Wanna play which games ?</h2>
+            <Link to="robot">Do you wont to create robot's</Link>
+            <Link to="clipboard">Or copy texts ?</Link>
+        </div>
+    )
+}
